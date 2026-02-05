562e21b96b8d3315900c2b52f48ac11010fb09b3
Sanjeev <154218692+Sanjeevpradeepit@users.noreply.github.com>
2026-02-05 10:57:58 +0530

    page-build-auth

--- page-build/server/src/app.module.ts
+++ page-build/server/src/app.module.ts
@@ -10,12 +10,11 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    AuthModule,
     PlanModule,
     TenantModule,
     UsersModule,
-    AuthModule,
     PrismaModule,
-    UsersModule,
     PagesModule,
     SectionsModule,
     FieldsModule,
